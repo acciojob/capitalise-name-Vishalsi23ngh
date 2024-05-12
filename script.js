3d5a07d2a73fb41fb75f019790f873e831d94a78
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-12 22:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-conts inputField = document.getElementById("fname");
+const inputField = document.getElementById("fname");
 
 inputField.addEventListener("blur" ,function() {
 	const value = inputField.value;
