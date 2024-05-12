bc463b4cc8fe037205b78456ae49e707f4c734db
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-12 22:15:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+conts inputField = document.getElementById("fname");
+
+inputField.addEventListener("blur" ,function() {
+	const value = inputField.value;
+	const outputValue = value.toUpperCase();
+	inputField.value = outputValue;
+})
+
